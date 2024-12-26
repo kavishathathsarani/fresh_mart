18b6b1352dbcf206a57f54054b2824a36e9a0959
HasaraR <hasara.randunu@gmail.com>
2024-12-26 12:11:16 +0530

    product

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
+
+import Product from "../components/Product";
+
 export default function Home() {
   return (
     <div >
-      
+      <Product />
     </div>
   );
 }
